aae39b1dee808826efad26f2823a441147aecb6f
Daigo Fujiwara <daigo@daigofujiwara.com>
2013-09-07 19:56:38 -0400

    edits and start of ghub pages

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* 
+	scripts.js
+	Written by Daigo Fujiwara (@DaigoFuji)
+	Date: PUBLISHEDDATE
+	use this: http://jshint.com/
+*/
+
+
+$(document).ready(function() {
+  // code goes here...
+
+});
